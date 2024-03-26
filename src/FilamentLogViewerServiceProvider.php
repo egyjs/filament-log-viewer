5d8243c9d38839595ce19b5cf53eeb9661611218
egyjs <egyjs@users.noreply.github.com>
2024-03-26 00:13:40 +0000

    Fix styling

--- src/FilamentLogViewerServiceProvider.php
+++ src/FilamentLogViewerServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Egyjs\FilamentLogViewer;
 
+use Egyjs\FilamentLogViewer\Commands\FilamentLogViewerCommand;
+use Egyjs\FilamentLogViewer\Testing\TestsFilamentLogViewer;
 use Filament\Support\Assets\AlpineComponent;
 use Filament\Support\Assets\Asset;
 use Filament\Support\Assets\Css;
@@ -13,8 +15,6 @@ use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Egyjs\FilamentLogViewer\Commands\FilamentLogViewerCommand;
-use Egyjs\FilamentLogViewer\Testing\TestsFilamentLogViewer;
 
 class FilamentLogViewerServiceProvider extends PackageServiceProvider
 {
